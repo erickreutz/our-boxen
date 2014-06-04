ceffee3ad7618d4cf4c0ca18d17306caac40031f
Eric <eric@airkrft.com>
2014-06-04 14:46:07 -0600

    Stuff

--- modules/people/manifests/erickreutz/osx.pp
+++ modules/people/manifests/erickreutz/osx.pp
@@ -1,19 +1,19 @@
 class people::erickreutz::osx {
   # osx settings
-  osx::global::disable_key_press_and_hold
-  osx::global::enable_keyboard_control_access
-  osx::global::expand_print_dialog
-  osx::global::expand_save_dialog
-  osx::global::disable_remote_control_ir_receiver
-  osx::dock::2d
-  osx::dock::dim_hidden_apps
-  osx::finder::show_all_on_desktop
-  osx::finder::unhide_library
-  osx::finder::show_hidden_files
-  osx::finder::enable_quicklook_text_selection
-  osx::universal_access::ctrl_mod_zoom
-  osx::disable_app_quarantine
-  osx::universal_access::enable_scrollwheel_zoom
-  osx::no_network_dsstores
-  osx::software_update
+  include osx::global::disable_key_press_and_hold
+  include osx::global::enable_keyboard_control_access
+  include osx::global::expand_print_dialog
+  include osx::global::expand_save_dialog
+  include osx::global::disable_remote_control_ir_receiver
+  include osx::dock::2d
+  include osx::dock::dim_hidden_apps
+  include osx::finder::show_all_on_desktop
+  include osx::finder::unhide_library
+  include osx::finder::show_hidden_files
+  include osx::finder::enable_quicklook_text_selection
+  include osx::universal_access::ctrl_mod_zoom
+  include osx::disable_app_quarantine
+  include osx::universal_access::enable_scrollwheel_zoom
+  include osx::no_network_dsstores
+  include osx::software_update
 }
