665c3b3b62a9e4687978b8fe397379bd0acb61bd
Eric <eric@airkrft.com>
2014-06-05 13:54:53 -0600

    dotenv to repo"

--- modules/projects/manifests/modnotebooks.pp
+++ modules/projects/manifests/modnotebooks.pp
@@ -1,8 +1,9 @@
 class projects::modnotebooks {
 
   boxen::project { 'modnotebooks':
-    ruby        => '2.1.1',
+    ruby        => '2.0.0',
     postgresql  => true,
+    dotenv      => true,
     redis       => true,
     nginx       => true,
     source      => 'git@bitbucket.org:erickreutz/mod-web.git'
