9eda727a0a777843e07db13549577922ea6a7d4a
Eric <eric@airkrft.com>
2014-06-04 22:09:14 -0600

    edits

--- manifests/site.pp
+++ manifests/site.pp
@@ -84,12 +84,6 @@ node default {
   ruby::version { '2.1.0': }
   ruby::version { '2.1.1': }
 
-  ruby_gem { 'bundler for all rubies':
-    gem          => 'bundler',
-    version      => '~> 1.5',
-    ruby_version => '*',
-  }
-
   # php
   class { 'php::global': version => '5.4.10' }
 
