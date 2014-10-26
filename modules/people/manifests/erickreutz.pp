a1595359dfdb0c95e910cab15744686469407a64
Eric Kreutzer <eric@airkrft.com>
2014-10-25 20:53:02 -0700

    Gemfile.lock and Puppetfile.lock regenerated

--- modules/people/manifests/erickreutz.pp
+++
@@ -1,9 +0,0 @@
-class people::erickreutz (
-  $my_username  = $people::erickreutz::params::my_username,
-  $my_homedir   = $people::erickreutz::params::my_homedir,
-  $my_sourcedir = $people::erickreutz::params::my_sourcedir,
-) inherits people::erickreutz::params {
-  include people::erickreutz::config
-  include people::erickreutz::applications
-  include people::erickreutz::osx
-}
