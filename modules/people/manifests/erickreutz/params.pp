f9a0b6325cb1cdd077c98720393d612a618ae91f
Eric Kreutzer <eric@lu.gg>
2015-05-31 13:07:10 -0700

    Merge upstream

--- modules/people/manifests/erickreutz/params.pp
+++
@@ -1,6 +0,0 @@
-class people::erickreutz::params {
-  # $::luser and $::boxen_srcdir come from Boxen's custom facts
-  $my_username  = $::boxen_user
-  $my_homedir   = "/Users/${my_username}"
-  $my_sourcedir = $::boxen_srcdir
-}
